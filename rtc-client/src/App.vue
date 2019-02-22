0137b240473f6a7b90f167f72faa8233616941a3
sombriks <sombriks@gmail.com>
2019-02-22 17:50:48 -0300

    shaping up sample app

--- rtc-client/src/App.vue
+++ rtc-client/src/App.vue
@@ -1,14 +1,46 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/lobby">Lobby</router-link> |
-      <router-link to="/match">Match</router-link>
-    </div> -->
-    <router-view />
+  <div>
+    <div class="box">
+      <v-stage @click="handleClick" :config="{width,height}">
+        <v-layer>
+          <v-circle v-for="g in $store.state.players" :key="g.sId" :config="g"></v-circle>
+          <!-- <v-circle :config="$store.state.guest"></v-circle> -->
+        </v-layer>
+      </v-stage>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: "catch-game",
+  data: _ => ({
+    width: 800,
+    height: 600
+  }),
+  created() {
+    this.res();
+    window.addEventListener("resize", this.res);
+  },
+  methods: {
+    res() {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+    },
+    handleClick(e) {
+      const { layerX, layerY } = e.evt;
+      const { sId, x, y } = this.$store.state.guest;
+      this.$store.commit("moveTo", { sId, layerX, layerY, x, y });
+    }
+  }
+};
+</script>
 
 <style>
+body {
+  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
 #app {
   font-family: "Courier", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,16 +48,7 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.box {
+  border: solid 1px black;
 }
 </style>
--- rtc-client/src/main.js
+++ rtc-client/src/main.js
@@ -1,14 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
 import VueKonva from "vue-konva";
 
 Vue.use(VueKonva);
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount("#app");
